a1a09caab1866b20ba79c9d67db79ebd6e29dd2e
Marko Malak Yaacoub <92307411+Mark-Yaacoub@users.noreply.github.com>
2023-10-23 09:38:24 +0300

    bh

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document);
-  await app.listen(3000);
+  await app.listen(5000);
 
 }
 bootstrap();
+  
